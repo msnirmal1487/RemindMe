ede4ddfec9c07b93a73c2be44dc8ad70f179e317
Subramanian, Nirmal <nirmal.subramanian@staples.com>
2019-07-26 13:19:43 -0400

    Setting up spoon client to take screenshots and give report for tests

--- app/src/androidTest/java/com/mytests/mobile/remindme/NoteCreationTest.java
+++ app/src/androidTest/java/com/mytests/mobile/remindme/NoteCreationTest.java
@@ -1,16 +1,23 @@
 package com.mytests.mobile.remindme;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 
+import androidx.core.app.ActivityCompat;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.jraska.falcon.Falcon;
 import com.mytests.mobile.remindme.model.BillInfo;
 import com.mytests.mobile.remindme.model.BillInfoList;
 import com.mytests.mobile.remindme.utilities.CacheDataManager;
 import com.mytests.mobile.remindme.utilities.PaymentFrequency;
+import com.squareup.spoon.Spoon;
+//import com.squareup.spoon.Spoon;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -37,14 +44,30 @@ public class NoteCreationTest {
     public ActivityTestRule<BillListActivity> billListActivityActivityTestRule =
             new ActivityTestRule<>(BillListActivity.class) ;
 
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+//    @Rule
+//    public final FalconSpoonRule falconSpoonRule = new FalconSpoonRule();
+
     @Test
     public void createNewNote(){
 
         PaymentFrequency paymentFrequency = PaymentFrequency.MONTHLY ;
+        Activity activity = billListActivityActivityTestRule.getActivity() ;
 
+//        FalconSpoonRule falconSpoonRule = new FalconSpoonRule();
+
+        // Round about way of doing "onView(withId(R.id.fab)).perform(click());"
 //        ViewInteraction fabNewBill = onView(withId(R.id.fab)) ;
 //        fabNewBill.perform(click());
+        Spoon.screenshot(activity, "App_Launch_Bill_List_Shown") ;
+//        falconSpoonRule.screenshot(activity, "App_Launch_Bill_List_Shown"); ;
+
         onView(withId(R.id.fab)).perform(click());
+
+        Spoon.screenshot(activity, "Bill_Shown") ;
 
         String testBillName = "Test Bill Name";
         String testBillNote = "Test Bill Note";
@@ -66,9 +89,12 @@ public class NoteCreationTest {
         onView(withId(R.id.spinner_frequency))
                 .check(matches(withSpinnerText(containsString(testPaymentFrequency.getFrequency()))));
 
+        Spoon.screenshot(activity, "All_Bill_Details_Entered") ;
         pressBack();
 
-        BillInfoList billInfoList = CacheDataManager.getInstance().readBillListCache(billListActivityActivityTestRule.getActivity());
+        Spoon.screenshot(activity, "Back_Pressed") ;
+
+        BillInfoList billInfoList = CacheDataManager.getInstance().readBillListCache(activity);
 
         if (billInfoList != null && billInfoList.getBills() != null && billInfoList.getBills().size() > 0){
             int lastIndex = billInfoList.getBills().size() -1 ;
@@ -78,6 +104,10 @@ public class NoteCreationTest {
             assertEquals(testBillName, billInfo.getBillName());
             assertEquals(testBillNote, billInfo.getNotes());
         }
+
+        assertNotNull(billInfoList);
+
+        Spoon.screenshot(activity, "Application_Complete") ;
     }
 
 }
