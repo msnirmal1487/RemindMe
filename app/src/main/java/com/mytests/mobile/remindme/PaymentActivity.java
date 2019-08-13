8711e0f56d34e5190c369eae718edbf5eb408e7d
Subramanian, Nirmal <nirmal.subramanian@staples.com>
2019-08-13 17:33:48 -0400

    Add Date picker dialog

--- app/src/main/java/com/mytests/mobile/remindme/PaymentActivity.java
+++ app/src/main/java/com/mytests/mobile/remindme/PaymentActivity.java
@@ -1,10 +1,9 @@
 package com.mytests.mobile.remindme;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,11 +11,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class PaymentActivity extends AppCompatActivity {
 
     private boolean isCancelling = false;
     private Context context;
+    private TextView txtDateView;
+    private Calendar calendar;
+    private int year;
+    private int month;
+    private int day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,53 @@ public class PaymentActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        txtDateView = (TextView) findViewById(R.id.txt_paid_date);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
+
+        txtDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setDate(view);
+            }
+        });
+
+    }
+
+    private void setDate(View view) {
+        showDialog(999);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        if(id == 999){
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+
+        return super.onCreateDialog(id);
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        txtDateView.setText(new StringBuilder().append(month).append("/")
+                .append(day).append("/").append(year));
     }
 
     @Override
